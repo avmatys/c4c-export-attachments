4e947c0fe0f90a4dda8c6cdc9f89e76043b98b7f
diprkpnk <1809841Diana>
2024-01-09 09:50:08 +0300

    Activity files

---
+++ activity.py
@@ -0,0 +1,106 @@
+import base64
+from datetime import datetime, date
+
+import requests
+import c4c
+import file_utils
+from main import ObjectType, FolderType
+
+
+def mapping_line(activity, attachment, att_path):
+    if attachment is None or activity is None or "Name" not in attachment:
+        return ""
+    att_name = attachment.get("Name", "")
+    att_uuid = attachment.get("ObjectID", "")
+    #att_size = attachment.get("SizeInkB", "")
+    att_creation_date = attachment.get("LastUpdatedOn", "")
+    # Get timestamp
+    timestamp = int(att_creation_date.replace("/Date(", "").replace(")/", ""))
+    # Calculate standard date format (year-month-day hours:minutes:seconds) for UTC+0
+    att_creation_date_form = datetime.utcfromtimestamp(timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')
+    att_creator = attachment.get("LastUpdatedBy", "")
+    att_link = attachment.get("DocumentLink", "")
+    att_mime = attachment.get("MimeType", "")
+    att_type = attachment.get("TypeCode", "")
+    #att_type_text = attachment.get("TypeCodeText", "")
+    uuid = activity.get("ObjectID", "")
+    id = activity.get("ID", "")
+    name = activity.get("SubjectName", "")
+    type = activity.get("TypeCode", "")
+    line = f"{att_path};{att_name};{uuid};{id};{name};{type};{att_uuid};{att_creator};{att_creation_date_form};{att_link};{att_mime};{att_type}"
+    return line
+
+
+def download_attachments(keys_path="/", file_folder="/", mapping_path="/", error_path="/", log_path="/", package=10):
+    keys = []
+    with open(keys_path, encoding="utf8") as fin:
+        c4c_client = requests.session()
+        lines = fin.readlines()
+        counter = len(lines)
+        alllines = counter
+        for i, line in enumerate(lines):
+            current_time = datetime.now()
+            print(f"{current_time} Thread {keys_path} Status {i}/{alllines}")
+            # Prepare line
+            line = line.rstrip()
+            # Iterate one by one
+            counter -= 1
+            # Skip header
+            if i == 0:
+                continue
+            # Get key
+            splitted_line = line.split(";")
+            uuid = None
+            id = None
+            if len(splitted_line) > 1:
+                uuid = splitted_line[0].replace("-", "")
+                id = splitted_line[1]
+            if uuid is None or id is None:
+                continue
+            # Store in collection
+            keys.append(uuid)
+            # Check if package should be processed
+            test = len(keys);
+            if len(keys) == package or counter == 0:
+                # Read data from C4C
+                data = c4c.get_data(keys, ObjectType.activity, c4c_client)
+                keys.clear()
+                # Some error during read - store into the error area
+                if data is None:
+                    file_utils.write_to_file(error_path, f"{line}, Check logs in c4c/api/logging.log")
+                    continue
+                # Iterate through data
+                for item in data:
+                    # Check if attachments exists
+                    if "ActivityAttachmentFolder" not in item or len(
+                            item["ActivityAttachmentFolder"]) == 0:
+                        file_utils.write_to_file(log_path, f"{line}; 0")
+                        continue
+                    # Save data into the file
+                    atts = item["ActivityAttachmentFolder"]
+                    id = item.get("ID",None)
+                    for att in atts:
+                        file_content = att.get('Binary', None)
+                        filename = att.get('Name', None)
+                        mime_code = att.get('MimeType', None)
+                        if file_content is None or filename is None:
+                            file_utils.write_to_file(error_path, f"{line}, Binary is not available")
+                            continue
+                        att_name = f"{id}_{filename}"
+                        att_path = f"{file_folder}/{att_name}"
+                        binary = base64.b64decode(file_content)
+                        # Skip links (urls)
+                        if mime_code is not None and len(mime_code) > 0:
+                           with open(att_path, 'wb') as f:
+                                f.write(binary)
+                        # Prepare mapping
+                        line = mapping_line(item, att, att_name)
+                        file_utils.write_to_file(mapping_path, f"{line}")
+                    # Save number of atts for customer
+                    file_utils.write_to_file(log_path, f"{line}; {len(atts)}")
+
+
+if __name__ == '__main__':
+    debug = 1
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
